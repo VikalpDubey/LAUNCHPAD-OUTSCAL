8686c68450ced877f92881b29000e44bf4acf2e2
VikalpDubey <94435665+VikalpDubey@users.noreply.github.com>
2022-07-04 05:55:47 +0530

    spawning Tank

---
+++ Assets/Scripts/TankSpwan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpwan : MonoBehaviour
+{
+    public GameObject Tank;
+    // Start is called before the first frame update
+    void Start()
+    {
+    Instantiate(Tank, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
